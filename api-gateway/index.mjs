48e334f9e252fddf57559f5a312120e4b14d5698
Deepak Sharma <stdeepak22@gmail.com>
2023-10-05 00:17:35 +0530

    update the dockerfile and docker compose file, to use the same node version 18.18 expose env variable for gateway configure lerna to install/run in single command.

--- api-gateway/index.mjs
+++ api-gateway/index.mjs
@@ -3,22 +3,22 @@ import gateway from 'fast-gateway';
 const server = gateway({
     routes: [
         {
-            prefix: '/test',
-            target: 'https://reqres.in/api/users?page=1'
+            prefix: '/test-git',
+            target: process.env.test_url || 'https://github.com/'
         },
         {
             prefix: '/sm',
-            target: 'http://c_sm/'
+            target: process.env.state_management_container_name || 'http://localhost:8090/'
         },
         {
             prefix: '/cart',
-            target: 'http://c_cart/',
+            target: process.env.cart_container_name || 'http://localhost:8081/'
         },
         {
             prefix: '/product',
-            target: 'http://c_prod/'
+            target: process.env.products_container_name || 'http://localhost:8082/'
         }
     ]
 },)
 
-server.start(9090);
+server.start(9090).then(() => console.log('Gateway Started!!'));
--- host/src/header/header.tsx
+++ host/src/header/header.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import {Component, ErrorInfo, ReactInstance, ReactNode} from 'react'
+import { Component, ErrorInfo, ReactInstance, ReactNode } from 'react'
 import classes from './header.module.css';
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 import { CartStateType, CartStateName } from 'cart'
-import {OtherSliceAction} from './../test-slice/OtherSlice'
+import { OtherSliceAction } from './../test-slice/OtherSlice'
 
-interface HeaderComponentProps{
+interface HeaderComponentProps {
     CartStateName: CartStateType;
     onClick: Function
 }
@@ -22,16 +22,16 @@ class _HeaderComponent extends Component<HeaderComponentProps> {
                 Host App
             </div>
             <div className={classes.menus}>
-                Others
+                [nav menu placeholder]
             </div>
             <div className={classes.cartInfo}>
-                <button className={classes.cartButton} onClick={()=>this.props.onClick()}>
-                    {this.props.CartStateName.items.map((c:any)=>c.qty).reduce((a:number,b:number)=>a+b, 0)} items
+                <button className={classes.cartButton} onClick={() => this.props.onClick()}>
+                    {this.props.CartStateName.items.map((c: any) => c.qty).reduce((a: number, b: number) => a + b, 0)} items
                 </button>
             </div>
         </div>
     }
 }
-export const HeaderComponent = connect((state:any)=>({CartStateName:state[CartStateName]}), dispatch=>({
-    onClick: ()=> dispatch(OtherSliceAction.toggleInlineCart(null))
+export const HeaderComponent = connect((state: any) => ({ CartStateName: state[CartStateName] }), dispatch => ({
+    onClick: () => dispatch(OtherSliceAction.toggleInlineCart(null))
 }))(_HeaderComponent);
